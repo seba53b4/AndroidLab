204159347ecce1398db86fca8de1084ab297adcc
martin.riani <martiniano13@gmail.com>
2022-09-28 21:34:25 -0300

    logout integration
    logout integration

--- app/src/main/java/com/example/noteapp/NoteActivity.kt
+++ app/src/main/java/com/example/noteapp/NoteActivity.kt
@@ -1,33 +1,33 @@
 package com.example.noteapp
 
 
-import android.app.Dialog
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import android.widget.LinearLayout
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.noteapp.adapters.NoteAdapter
 import com.example.noteapp.databinding.ActivityNoteScreenBinding
 import com.google.firebase.Timestamp
-import java.util.*
-import com.example.noteapp.R
 
 
 class NoteActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityNoteScreenBinding
     lateinit var recyclerView: RecyclerView
+    private val sharedPref = SharedPreferencesService()
+    private val firebaseService = FirebaseService()
+    private lateinit var userEmail: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNoteScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
         recyclerView = binding.recyclerviewNotes
+
+        userEmail = sharedPref.getUserLogin(this@NoteActivity) ?: ""
 
         val notes = mutableListOf<Note>()
         notes.add(Note("1","a@mail.com","Lista Super de cosas para ver si se rompe","Comprar pan", null, Timestamp.now(),false));
@@ -54,12 +54,32 @@ class NoteActivity : AppCompatActivity() {
 
         binding.logoutBtn.setOnClickListener{
             // desloguearse
-            val intent = Intent(applicationContext,LoginActivity::class.java)
-            startActivity(intent)
-            finish()
+            showDialog(getString(R.string.dialog_logout_title), getString(R.string.dialog_logout_msg))
         }
 
 
     }
+
+    private fun showDialog(title: String, message: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setPositiveButton(getString(R.string.dialog_accept), confirmLogOut)
+        builder.setNegativeButton(getString(R.string.dialog_cancel), cancelLogOut)
+        builder.show()
+    }
+
+    private val confirmLogOut = { dialog: DialogInterface, _: Int ->
+        firebaseService.deleteAllNotes(userEmail)
+        sharedPref.removeUserLogin(this@NoteActivity)
+        val intent = Intent(applicationContext,LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private val cancelLogOut = { dialog: DialogInterface, _: Int ->
+        dialog.cancel()
+    }
+
 
 }
