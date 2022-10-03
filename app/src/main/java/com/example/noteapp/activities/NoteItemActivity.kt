c990743840e5437b3cf0f6e01c6f146282845f90
seba53b4 <seba5364@hotmail.com>
2022-10-02 22:55:16 -0300

    NoteItemActivity - Delete button logics added

--- app/src/main/java/com/example/noteapp/activities/NoteItemActivity.kt
+++ app/src/main/java/com/example/noteapp/activities/NoteItemActivity.kt
@@ -1,10 +1,13 @@
 package com.example.noteapp.activities
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.noteapp.R
 import com.example.noteapp.adapters.ItemListAdapter
 import com.example.noteapp.adapters.NoteAdapter
 import com.example.noteapp.databinding.ActivityNoteItemBinding
@@ -35,14 +38,76 @@ class NoteItemActivity : AppCompatActivity() {
         binding.descriptionTextView.text = note.body
         binding.dateTextView.text = getDateTime(note.date)
 
-        val manager =  GridLayoutManager(this, 1, GridLayoutManager.VERTICAL, false);
-        recyclerView.layoutManager = manager;
-        recyclerView.adapter = ItemListAdapter(note.itemList!!)
+        if (note.isMultiline) {
+            val manager = GridLayoutManager(this, 1, GridLayoutManager.VERTICAL, false);
+            recyclerView.layoutManager = manager;
+            recyclerView.adapter = ItemListAdapter(note.itemList!!)
+        }
+
+        binding.deleteButton.setOnClickListener {
+            showDialog(
+                getString(R.string.dialog_note_delete_title),
+                getString(R.string.dialog_confirm_action_title),
+                confirmAdd, cancel)
+        }
+
+        binding.btnBackToNotes.setOnClickListener {
+            backToNotes()
+        }
     }
 
-    private fun setNote(){
-        val str = intent.extras?.getString("note")
+    private val confirmAdd = { dialog: DialogInterface, _: Int ->
+        firebaseService.deleteNote(note,::deleteNoteSuccess,
+            ::deleteNoteFailure)
+    }
+
+    private val confirm = { dialog: DialogInterface, _: Int ->
+        backToNotes()
+    }
+
+    private val cancel = { dialog: DialogInterface, _: Int ->
+        dialog.cancel()
+    }
+
+    private fun showDialog(
+        title: String,
+        message: String,
+        listenerAccept: DialogInterface.OnClickListener?,
+        listenerCancel: DialogInterface.OnClickListener?) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setPositiveButton(getString(R.string.dialog_accept), listenerAccept)
+        if (listenerCancel != null) {
+            builder.setNegativeButton(getString(R.string.dialog_cancel), listenerCancel)
+        }
+        builder.show()
+    }
+
+    private fun backToNotes() {
+        val intent = Intent(applicationContext, NoteActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun deleteNoteSuccess() {
+        showDialog(
+            getString(R.string.dialog_note_delete_ok_title),
+            getString(R.string.dialog_note_delete_ok_msg),
+            confirm, null
+        )
+    }
+
+    private fun deleteNoteFailure() {
+        showDialog(
+            getString(R.string.dialog_note_delete_error_title),
+            getString(R.string.dialog_note_save_error_msg),
+            null, null
+        )
+    }
+    private fun setNote() {
+        val jSON= intent.extras?.getString("note")
         val gson = Gson()
-        note = gson.fromJson(str,Note::class.java)
+        note = gson.fromJson(jSON,Note::class.java)
     }
 }
